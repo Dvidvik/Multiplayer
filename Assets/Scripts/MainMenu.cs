5401b1c2d4fe2f2063b0ab03db35cfa630ba3b6c
Jan Karban <jan.karban2005@gmail.com>
2023-09-03 18:35:22 +0200

    Creating and Joining

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+
+
+    public void CreateRoom()
+    {
+        //Set maximum players to 2
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+
+        //Creates the room with options we defined above
+        PhotonNetwork.CreateRoom(createInput.text, roomOptions);
+    }
+
+
+    //
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    //Automaticly loads the game scene "Game"
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+
+}
